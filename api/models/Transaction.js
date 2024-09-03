2c61aa93b0ae6b0d92b42af181d1b10eb0d14900
ManojM-656565 <gamermanoj00@gmail.com>
2024-09-03 06:15:02 +0530

    Initial commit

---
+++ api/models/Transaction.js
@@ -0,0 +1,14 @@
+
+const moongoose=require('mongoose');
+const {Schema,model}=moongoose;
+
+const TransactionSchema=new Schema({
+    price:{type:Number,required:true},
+    name:{type:String,required:true},
+    description:{type:String,required:true},
+    datetime:{type:Date,required:true},
+});
+
+const TransactionModel=model('Transaction',TransactionSchema);
+
+module.exports=TransactionModel;
